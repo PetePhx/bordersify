aaac503885e465400366f3d48ca17a9cd8a33b8c
Peter <pete0phx@yandex.com>
2019-01-01 09:48:01 -0700

    1px border around all elements

--- borderify.js
+++ borderify.js
@@ -1,1 +1,5 @@
-document.body.style.border = "5px solid red";
+const arrayize = pseudoArr => Array.prototype.slice.apply(pseudoArr);
+
+arrayize(document.body.getElementsByTagName("*")).forEach(elm => {
+  elm.style.border = "1px solid red";
+});
